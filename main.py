d015e786bbfdec6b1b838a48239b3378d45082ac
Oleg-Pashchenko <pickpar@ya.ru>
2023-05-19 13:13:14 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET", 'POST'])
+def index_page():
+    addr = str(request.remote_addr)
+    result = {'place': 'None'}
+    if request.method == 'POST':
+        query = request.form.to_dict()['query']
+        result = {'place': "1 стилаж, 2 ряд, 3 коробка", 'item': 'Зеленая горловина'}
+
+    return render_template('index.html', result=result, addr=addr)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
